ddc5be5f57997594b7f7dfed451a029396cbc5cd
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-18 18:14:06 +0300

    feat: using class validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'warn'],
   });
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     console.log(`Server is running on on port: ${PORT}...`);
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,12 @@
+import { IsString, MinLength } from 'class-validator';
 import { TaskStatus } from '../interface/task.interface';
 
 export class CreateTaskDto {
+  @IsString()
+  @MinLength(5)
   title: string;
+
+  @IsString()
+  @MinLength(10)
   description: TaskStatus;
 }
