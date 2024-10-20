1ff3c1e56b01e0a82b495246e857f4f9adfd5ee1
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-20 13:10:01 +0300

    feat: setting jwt module and passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      },
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
