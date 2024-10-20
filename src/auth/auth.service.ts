a74a6d4fa5dd681082b4406fec7b6fbafca13515
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-20 09:01:17 +0300

    feat: create users entity

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async signup() {
+    console.log(await this.usersRepository.find());
+    return 'This action adds a new user';
+  }
+
+  async login() {
+    return 'This action returns a token';
+  }
+}
