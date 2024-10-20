39bd05e403c138dd78a15d3baacb2cde72d7bc95
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-20 17:05:13 +0300

    feat: use class transformer and nestjs interceptors | serialize user data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,7 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
